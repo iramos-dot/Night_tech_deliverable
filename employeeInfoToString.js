e911c7609f703cb32c0eb703c61004446c655a3e
Iesha Ramos <iramos@icstars.org>
2024-08-13 19:35:12 -0500

    Ryan Deliverable

---
+++ employeeInfoToString.js
@@ -0,0 +1,16 @@
+// These two functions are broken in some way...
+// What went wrong? What needs to be fixed?
+// How do we know if it's fixed or not?
+// Hint: using a console.log may be useful...
+
+export function employeeInfoToString(employee) {
+    let fullName = generateFullName(employee.first_name, employee.middle_name, employee.last_name);
+
+
+    return `${fullName}\nDepartment: ${employee.department} | Salary: ${ employee.salary}`;
+}
+
+function generateFullName(first_name, middle_name, last_name) {
+    return `${first_name}${middle_name ? " " + middle_name : ""} ${last_name}`
+}
+ 
